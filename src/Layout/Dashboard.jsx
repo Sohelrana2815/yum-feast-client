457cd57c3556939e0feafa16a68b808dfe9897f2
Sohelrana2815 <sohel152302@gmail.com>
2024-09-11 19:34:16 +0600

    dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,20 +1,70 @@
 import { NavLink, Outlet } from "react-router-dom";
-
+import {
+  FaCalendar,
+  FaCreditCard,
+  FaHome,
+  FaShoppingCart,
+  FaStar,
+} from "react-icons/fa";
+import { FaBookBookmark } from "react-icons/fa6";
 const Dashboard = () => {
   return (
-    <div className="flex">
+    <div className="flex ">
       {/* Dashboard sidebar */}
-      <div className="w-72 min-h-screen bg-[#d1a054]">
-        <ul className="menu">
+      <div className="w-72 min-h-screen p-4 bg-[#d1a054]">
+        <ul className="menu uppercase space-y-5">
           <li>
-            <NavLink to="/dashboard/cart">My Cart</NavLink>
+            <NavLink className="py-3" to="/dashboard/userHome">
+              <FaHome />
+              USER HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="py-3" to="/dashboard/reservation">
+              <FaCreditCard />
+              PAYMENT HISTORY
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="py-3" to="/dashboard/paymentHistory">
+              <FaShoppingCart />
+              MY CART
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="py-3" to="/dashboard/cart">
+              <FaStar />
+              add review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="py-3" to="/dashboard/myBooking">
+              <FaCalendar />
+              my booking
+            </NavLink>
+          </li>
+          <div className="divider"></div>
+
+          <li>
+            <NavLink className="py-3" to="/">
+              <FaHome className="text-xl" />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="py-3" to="/order/salad">
+              <FaBookBookmark className="text-xl" />
+              Menu
+            </NavLink>
           </li>
         </ul>
       </div>
 
       {/* Dashboard  content */}
       <div className="flex-grow">
-        <Outlet />
+        <div className="max-w-screen-xl mx-auto mt-10">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- src/Pages/Dashboard/Cart.jsx
+++ src/Pages/Dashboard/Cart.jsx
@@ -1,7 +1,17 @@
+import useCart from "../../hooks/useCart";
+
 const Cart = () => {
+  const [cart] = useCart();
+
+  const totalPrice = cart.reduce(
+    (acc, currentPrice) => acc + currentPrice.price,
+    0
+  );
   return (
-    <div>
-      <h2>MY CART</h2>
+    <div className="flex justify-evenly">
+      <h2 className="text-4xl font-semibold">Total orders : {cart.length}</h2>
+      <h2 className="text-4xl font-semibold">Total Price : {totalPrice}$</h2>
+      <button className=" btn btn-primary text-white">Pay</button>
     </div>
   );
 };
